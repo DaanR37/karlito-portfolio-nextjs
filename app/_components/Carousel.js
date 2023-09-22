accb3ddf2adcae8edf073477902fbe9332fd9032
Daan Roelofs <daanroelofs599@gmail.com>
2023-09-22 13:16:44 +0200

    contact page flex containers

--- app/_components/Carousel.js
+++ app/_components/Carousel.js
@@ -4,7 +4,6 @@ import Image from "next/image";
 import ImageComp from "./ImageComp";
 
 export default function Carousel({ images, interval }) {
-  // const [currentIndex, setCurrentIndex] = useState(0);
   const [activeIndex, setActiveIndex] = useState(0);
 
   useEffect(() => {
@@ -41,53 +40,5 @@ export default function Carousel({ images, interval }) {
         />
       ))}
     </div>
-
-
-    // <div className="carousel relative rounded-[10px] h-[400px] max-w-[650px] mx-auto overflow-hidden">
-    //   <Image
-    //     key={currentIndex}
-    //     src={`/images${images[currentIndex]}`}
-    //     alt="carousel image"
-    //     width={300}
-    //     height={300}
-    //     className="w-[99%] h-[99%] rounded-[8px]"
-    //   />
-    //   <div className="slide_direction flex justify-between">
-    //     <div className="left absolute h-[25px] w-[25px] bg-dark text-light py-2 px-2 mt-0 mb-[20px] 
-    //     top-0 bottom-0 mx-auto my-[10px] left-0"
-    //       onClick={handlePrevious}>
-    //       <svg
-    //         xmlns="http://www.w3.org/2000/svg"
-    //         height="20"
-    //         viewBox="0 96 960 960"
-    //         width="20"
-    //       >
-    //         <path d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z" />
-    //       </svg>
-    //     </div>
-    //     <div className="right absolute h-[25px] w-[25px] bg-dark text-light py-2 px-2 mt-0 mb-[20px] 
-    //     top-0 bottom-0 mx-auto my-[10px] right-0"
-    //       onClick={handleNext}>
-    //       <svg
-    //         xmlns="http://www.w3.org/2000/svg"
-    //         height="20"
-    //         viewBox="0 96 960 960"
-    //         width="20"
-    //       >
-    //         <path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
-    //       </svg>
-    //     </div>
-    //   </div>
-    //   <div className="indicator flex mt-[20px] justify-center gap-[20px]">
-    //     {images.map((_, index) => (
-    //       <div
-    //         key={index}
-    //         className={`dot ${currentIndex === index ? "active" : ""}
-    //         w-[15px] h-[15px] rounded-[50%] bg-[whitesmoke]`}
-    //         onClick={() => handleDotClick(index)}
-    //       ></div>
-    //     ))}
-    //   </div>
-    // </div>
   );
 };
--- app/contact/page.js
+++ app/contact/page.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 export default function Page() {
   return (
-    <main id='contact' className='contact relative w-full h-[100vh] bg-[purple]'>
-      <div></div>
+    <main id='contact' className='contact relative w-full h-[100vh] flex justify-center'>
+      <div className='absolute w-full h-full bg-dark'>
+        <div className='absolute w-1/2 h-full left-0 bg-[#3d87c7]'></div>  {/* flex items-center justify-center */}
+        <div className='absolute flex items-center justify-center w-full h-full'>
+          <div className='absolute w-[75vw] h-[70vh] rounded-[5px] bg-dark'>
+
+            <div className='absolute w-1/2 h-full right-0 bg-[#3d87c7]'></div>
+
+          </div>
+        </div>
+
+      </div>
     </main>
   )
 };
