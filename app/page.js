64fc4d29e2b169c1057ebb1b44d6b4de66153c02
Daan Roelofs <daanroelofs599@gmail.com>
2023-09-25 16:54:00 +0200

    defining routing and scroll 9

--- app/page.js
+++ app/page.js
@@ -2,23 +2,24 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
 import LocomotiveScrollSetup from './components/LocomotiveScrollSetup';
-// import HomePage from "./homepage/page";
+import HomePage from "./homepage/page";
+import ContactPage from "./contact/page";
 
 const DynamicHeader = dynamic(() => import('./components/header/Header'), {
   ssr: false,
 });
-const DynamicHomepage = dynamic(() => import('./homepage/page'), {
-  ssr: false,
-});
+// const DynamicHomepage = dynamic(() => import('./homepage/page'), {
+//   ssr: false,
+// });
 const DynamicAbout = dynamic(() => import('./about/page'), {
   ssr: false,
 });
 const DynamicProjects = dynamic(() => import('./projects/page'), {
   ssr: false,
 });
-const DynamicContact = dynamic(() => import('./contact/page'), {
-  ssr: false,
-});
+// const DynamicContact = dynamic(() => import('./contact/page'), {
+//   ssr: false,
+// });
 
 
 export default function Home() {
@@ -26,11 +27,12 @@ export default function Home() {
     <main className="w-full">
       <LocomotiveScrollSetup />
       <DynamicHeader />
-      {/* <HomePage /> */}
-      <DynamicHomepage />
+      <HomePage />
+      {/* <DynamicHomepage /> */}
       <DynamicAbout />
       <DynamicProjects />
-      <DynamicContact />
+      {/* <DynamicContact /> */}
+      <ContactPage />
     </main>
   )
 };
